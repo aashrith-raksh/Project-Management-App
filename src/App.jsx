bffd8556f200ed2f888685626bb2596b1087ad11
Aashrith Raksh <aashrith.raksh@gmail.com>
2024-05-23 16:28:42 +0530

    created a "NoProjectSelected" component that displays the screen when the user has not selected any project to work on

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import NewProject from "./components/NewProject";
 import SideBar from "./components/SideBar";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     
     <main className="h-screen my-8 flex gap-8">
       <SideBar/>
-      <NewProject/>
+      {/* <NewProject/> */}
+      <NoProjectSelected/>
     </main>
   );
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,11 @@
+import Button from "./Button";
+
 export default function SideBar(){
     return <>
         <aside className="px-8 py-16 w-1/3 bg-stone-900 text-stone-300 md:w-72 rounded-r-xl">
             <h1 className="mb-8 uppercase font-bold md:text-xl text-stone-200">Projects</h1>
             <div>
-                <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-200">+ Add Project</button>
+                <Button>+ Add Project</Button>
             </div>
             <ul></ul>
         </aside>
